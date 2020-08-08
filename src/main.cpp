5781a37b3034af71566ca365d91e00efd7a1fdd5
Floris <florismeccanici@gmail.com>
2020-08-08 22:50:45 +0200

    working multiple circles drawing

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,20 @@ int main()
 
     // Vector2d vector2d(1.0, 1.0);
     // std::cout << vector2d.getX() << std::endl;
-    int n_circles = 1;
+    int n_circles = 5;
     vector<Circle> balls(n_circles);
     vector<sf::CircleShape> balls_to_draw(n_circles);
 
     for (int i = 0; i < n_circles; i++)
     {   
-        float radius = i;
-        float x = 1.0; 
+        float radius = 5.0;
+        float x = 1.0 + i * 10.0; 
         float y = 1.0;
         Vector2d position = Vector2d(x, y);
         
         balls[i].setCircle(radius, position);
         
-        balls_to_draw[i].setOrigin(position.getX(), position.getY());
+        balls_to_draw[i].setPosition(position.getX(), position.getY());
         balls_to_draw[i].setRadius(radius);
         balls_to_draw[i].setFillColor(sf::Color::Green);
     }
@@ -48,16 +48,16 @@ int main()
             // sf::CircleShape ball(balls[i].getRadius());
             // sf::CircleShape ball(10.0);
 
-            cout<<balls_to_draw[i].getPosition().getX()<<endl;
-            cout<<balls_to_draw[i].getPosition().getX()<<endl;
-            cout<<balls_to_draw[i].getRadius()<<endl;
+            cout<<balls[i].getPosition().getX()<<endl;
+            cout<<balls[i].getPosition().getX()<<endl;
+            cout<<balls[i].getRadius()<<endl;
 
             cout<<"\n"<<endl;
 
             // ball.setOrigin(balls[i].getPosition().getX(), balls[i].getPosition().getY());
-            ball.setOrigin(10.0, 10.0);
+            // ball.setOrigin(10.0, 10.0);
 
-            window.draw(ball);
+            window.draw(balls_to_draw[i]);
         }
         window.display();
     }
