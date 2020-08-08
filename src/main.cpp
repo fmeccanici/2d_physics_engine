df04903124838d4f413b43bec77debae24ed1b1c
Floris <florismeccanici@gmail.com>
2020-08-08 22:59:20 +0200

    random ball location and radius working

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,21 @@ using namespace std;
 
 int main()
 {   
-
-    // Vector2d vector2d(1.0, 1.0);
-    // std::cout << vector2d.getX() << std::endl;
     int n_circles = 5;
     vector<Circle> balls(n_circles);
     vector<sf::CircleShape> balls_to_draw(n_circles);
 
     for (int i = 0; i < n_circles; i++)
     {   
-        float radius = 5.0;
-        float x = 1.0 + i * 10.0; 
-        float y = 1.0;
+        float random_radius = rand() % 50 + 1;
+        float radius = random_radius;
+
+        float random_x_position = rand() % 700 + 1;
+        float random_y_position = rand() % 700 + 1;
+
+        float x = random_x_position; 
+        float y = random_y_position;
+
         Vector2d position = Vector2d(x, y);
         
         balls[i].setCircle(radius, position);
@@ -45,20 +48,9 @@ int main()
         
         for (int i = 0; i < balls_to_draw.size(); i++)
         {
-            // sf::CircleShape ball(balls[i].getRadius());
-            // sf::CircleShape ball(10.0);
-
-            cout<<balls[i].getPosition().getX()<<endl;
-            cout<<balls[i].getPosition().getX()<<endl;
-            cout<<balls[i].getRadius()<<endl;
-
-            cout<<"\n"<<endl;
-
-            // ball.setOrigin(balls[i].getPosition().getX(), balls[i].getPosition().getY());
-            // ball.setOrigin(10.0, 10.0);
-
             window.draw(balls_to_draw[i]);
         }
+
         window.display();
     }
 
