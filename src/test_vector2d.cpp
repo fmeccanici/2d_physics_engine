3251b91aeb70f58ee25b35ad756175539b2e0a23
Floris <florismeccanici@gmail.com>
2020-08-13 20:56:04 +0200

    unit tests working :D

--- src/test_vector2d.cpp
+++ src/test_vector2d.cpp
@@ -1,4 +1,19 @@
 #include <cppunit/TestFixture.h>
+#include <cppunit/TestCase.h>
+#include <iostream>
+#include <string>
+#include <list>
+#include <cppunit/TestFixture.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/XmlOutputter.h>
+#include <netinet/in.h>
 
 #include <vector2d.h>
 
@@ -8,8 +23,9 @@ using namespace std;
 class TestVector2d : public CppUnit::TestFixture
 {
     CPPUNIT_TEST_SUITE(TestVector2d);
-    CPPUNIT_TEST(testConstructor);
+    // CPPUNIT_TEST(testDefaultConstructor);
     CPPUNIT_TEST(testAddition);
+    CPPUNIT_TEST_SUITE_END();
 
 public:
     void setUp(void);
@@ -18,4 +34,68 @@ public:
 protected:
     void testConstructor(void);
     void testAddition(void);
+
+private:
+    Vector2d *testVector2d_1;
+    Vector2d *testVector2d_2;
+
+};
+
+// void TestVector2d::testDefaultConstructor(void)
+// {
+//     Vector2d testVector2d;
+//     CPPUNIT_ASSERT(0 == testVector2d.getX());
+//     CPPUNIT_ASSERT(0 == testVector2d.getY());
+// }
+
+void TestVector2d::testAddition(void)
+{
+    Vector2d testVector2d_1(2,2);
+    Vector2d testVector2d_2(3,3);
+
+    CPPUNIT_ASSERT(5 == (testVector2d_1 + testVector2d_2).getX());
+    CPPUNIT_ASSERT(5 == (testVector2d_1 + testVector2d_2).getY());
+
+}
+
+void TestVector2d::setUp(void)
+{
+    testVector2d_1 = new Vector2d();
+    testVector2d_2 = new Vector2d();
+
+}
+
+void TestVector2d::tearDown(void)
+{
+    delete testVector2d_1;
+    delete testVector2d_2;
+
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION( TestVector2d );
+
+int main(int argc, char* argv[])
+{
+    CPPUNIT_NS::TestResult testresult;
+    CPPUNIT_NS::TestResultCollector collectedresults;
+
+    testresult.addListener (&collectedresults);
+
+    CPPUNIT_NS::BriefTestProgressListener progress;
+    testresult.addListener (&progress);
+
+    CPPUNIT_NS::TestRunner testrunner;
+
+    testrunner.addTest (CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest ());
+
+    testrunner.run(testresult);
+
+    CPPUNIT_NS::CompilerOutputter compileroutputter(&collectedresults, std::cerr);
+    compileroutputter.write ();
+    
+    ofstream xmlFileOut("cppTestBasicMathResults.xml");
+    XmlOutputter xmlOut(&collectedresults, xmlFileOut); 
+    xmlOut.write();
+
+    return collectedresults.wasSuccessful() ? 0 : 1;
 }
